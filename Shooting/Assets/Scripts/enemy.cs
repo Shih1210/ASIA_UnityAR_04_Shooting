deb7c4e02b2195bf1de09d6dbf44acdd262136c9
ayumu5826 <61813232+ayumu5826@users.noreply.github.com>
2020-06-30 21:20:19 +0800

    Final

---
+++ Shooting/Assets/Scripts/enemy.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class enemy : MonoBehaviour
+{
+    [Header("速度"), Range(0, 10)]
+    public float speed = 1.5f;
+    [Header("停止距離"), Range(0, 5)]
+    public float stopDistance = 2;
+    [Header("追蹤目標名稱")]
+    public string targetName = "Pumpkin_02";
+    [Header("爆炸效果")]
+    public GameObject explosion;
+    [Header("傷害"),Range(10,100)]
+    public float damage;
+
+    private Transform target;
+    private NavMeshAgent nma;
+
+    private void Start()
+    {
+        nma = GetComponent<NavMeshAgent>();
+        nma.speed = speed;
+        nma.stoppingDistance = stopDistance;
+
+        target = GameObject.Find(targetName).transform;
+        nma.SetDestination(target.position);
+    }
+
+    private void CreateEffect()
+    {
+        GameObject expl = Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(expl, 0.5f);
+        Destroy(gameObject);
+    }
+
+    private void Track()
+    {
+        nma.SetDestination(target.position);
+
+        if(nma.remainingDistance <= stopDistance)
+        {
+            CreateEffect();
+            target.GetComponent<pumpkin_02>().Damage(damage);
+        }
+    }
+
+
+
+    private void ClickAndDead()
+    {
+        CreateEffect();
+        pumpkin_02.target = transform;
+    }
+    private void OnMouseDown()
+    {
+        ClickAndDead();
+    }
+    private void Update()
+    {
+        Track();
+    }
+}
---
+++ Shooting/Assets/Scripts/pumpkin_02.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class pumpkin_02 : MonoBehaviour
+{
+    public static Transform target;
+
+    [Header("南瓜旋轉部位")]
+    public Transform pumpkin_02Rotation;
+    [Header("閃爍")]
+    public ParticleSystem psShine;
+    [Header("血量"), Range(100, 500)]
+    public float hp;
+    [Header("血條")]
+    public Image hpBar;
+    [Header("結束畫面")]
+    public GameObject final;
+    [Header("數量")]
+    public Text textCount;
+
+    private int count;
+    private float hpMax;
+
+    private void Start()
+    {
+        hpMax = hp;
+    }
+
+    private void Update()
+    {
+        Attack();
+    }
+
+    private void Attack()
+    {
+        if(target && hp > 0)
+        {
+            Vector3 pos = target.position;
+            pos.y = pumpkin_02Rotation.position.y;
+            psShine.Play();
+
+            pumpkin_02Rotation.LookAt(pos);
+
+            count++;
+
+        }
+    }
+
+    public void Damage(float damage)
+    {
+        hp -= damage;
+        hpBar.fillAmount = hp / hpMax;
+
+        if (hp <= 0)
+        {
+            final.SetActive(true);
+            textCount.text = "擊殺怪物數量:" + count;
+        }
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene("遊戲場景");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
